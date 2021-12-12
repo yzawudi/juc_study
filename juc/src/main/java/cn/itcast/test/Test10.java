253c4978d1f05d68111ba6201754a61343cf532d
yzawudi <3065413255@qq.com>
2021-12-12 21:35:58 +0800

    update

---
+++ juc/src/main/java/cn/itcast/test/Test10.java
@@ -0,0 +1,39 @@
+package cn.itcast.test;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j(topic = "c.Test10")
+public class Test10 {
+    public static void main(String[] args) {
+        Object A = new Object();
+        Object B = new Object();
+        new Thread(() -> {
+            synchronized (A) {
+                log.debug("lock A");
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (B) {
+                    log.debug("lock B");
+                }
+            }
+        },"t1").start();
+        new Thread(() -> {
+            synchronized (B) {
+                log.debug("lock B");
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (A) {
+                    log.debug("lock A");
+                }
+            }
+        },"t2").start();
+    }
+}
